73b36d83acd5adadb4bd4c6461cd253f882462e8
Sang min gyu <ssangmg@gmail.com>
2023-08-02 17:47:35 +0900

    fixExcelCalculate

--- Django/ExcelCalculate/main/views.py
+++ Django/ExcelCalculate/main/views.py
@@ -7,7 +7,15 @@ from sendEmail.views import *
 
 # Create your views here.
 def index(request):
-    return render(request, "main/index.html")
+    # 로그인된 사용자만 접근
+    # 조건문 : 사용자의 정보가 세션에 존재하면 메인(=서비스) 화면으로 출력
+    #         만약, 사용자의 정보가 세션에 없다면 로그인 화면으로 출력
+    if 'user_name' in request.session.keys():
+        return render(request, 'main/index.html') # 사용자의 세션 정보가 담겨져있는 상태에서의 index.html
+    else:
+        return redirect('main_signin')
+
+    #return render(request, "main/index.html") # 아무런 세션 정보가 없는 index.html
 
 def signup(request):
     return render(request, "main/signup.html")
@@ -45,6 +53,21 @@ def join(request):
 def signin(request):
     return render(request, "main/signin.html")
 
+def login(request):
+    # 로그인된 사용자만 이용할 수 있도록 구현
+    # 이 때, 현재 사용자가 로그인된 사용자인지 판단하기 위해 세션 사용 From (verify함수에서 만든 세션)
+    # 세션 처리 진행
+    loginEmail = request.POST['loginEmail']
+    loginPW = request.POST['loginPW']
+    user = User.objects.get(user_email=loginEmail)
+    if user.user_password == loginPW :
+        request.session['user_name'] = user.user_name # 사용자가 회원가입 시, 입력한 정보
+        request.session['user_email'] = user.user_email # 사용자가 회원가입 시, 입력한 정보
+        return redirect('main_index')
+    else:
+        # 로그인 실패, 정보가 다름
+        return redirect("main_loginFail")
+
 def verifyCode(request):
     return render(request, "main/verifyCode.html")
 
@@ -81,4 +104,16 @@ def verify(request):
     # return redirect('main_index') 인증이 완료되면 메인 화면으로 보내줌
 
 def result(request):
-    return render(request, "main/result.html")
+    if 'user_name' in request.session.keys():
+        return render(request, 'main/result.html') # 사용자의 세션 정보가 담겨져있는 상태에서의 index.html
+    else:
+        return redirect('main_signin')
+    
+def logout(request):
+    # 로그아웃의 개념 : 세션 정보를 삭제하는 것
+    # 파이썬에서 객체를 지울 때
+    del request.session['user_name']
+    del request.session['user_email']
+
+    return redirect('main_signin')
+    
